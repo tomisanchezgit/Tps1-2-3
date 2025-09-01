dafd1da94cbb23af3f404cc869e594cf847697cf
tomisanchezgit <tomassanchez2018@gmail.com>
2025-09-01 12:01:05 -0300

    tp1Terminado

---
+++ TP1/Vista/ej8.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="./Action/Ej8Cine.php" method="POST">
+        
+    <label for="edad">Edad: </label>
+    <input type="number" name="edad" id="edad">
+    <br><br>
+    <label for="Estudios">¿Cuenta con estudios?</label>
+    <br>
+    <label for="si">si</label>
+    <input type="radio" name="estudios" value="si" >
+    <label for="no">no</label>
+    <input type="radio" name="estudios" value="no">
+    <br>
+    <input type="submit" value="Enviar">
+
+    </form>
+
+
+
+</body>
+</html>
